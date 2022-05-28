304757609f7c8264d1904dabe4a049098914059a
Blaire <ogoh_blessing@yahoo.com>
2022-05-28 13:52:15 +0100

    Add static files

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h2>BLAIRE</h2>
+    </div>
+    `
 })
-export class AppComponent {
+export class EventsAppComponent {
   title = 'angular-jcoop';
 }
