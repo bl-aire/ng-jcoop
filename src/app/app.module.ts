7ad3927f8482b923ba2ae22c1e4e7e23c60f71d1
Blaire <ogoh_blessing@yahoo.com>
2022-06-09 20:22:42 +0100

    Wrap third party service(Toastr)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
@@ -7,9 +9,14 @@ import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.shared';
 import { NavComponent } from './nav/navbar.component';
 
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   declarations: [
     EventsAppComponent,
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "./shared/event.shared";
 
+declare let toastr:any
+
 @Component({
     selector: 'events-list',
     template: `
@@ -9,7 +11,7 @@ import { EventService } from "./shared/event.shared";
         <hr>
         <div class="row">
             <div  *ngFor="let event of events" class="col-md-5 ">
-                <event-thumbnail [event]="event"></event-thumbnail>
+                <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
             </div>
         </div>
     </div>`
@@ -23,5 +25,9 @@ export class EventsListComponent implements OnInit {
 
     ngOnInit() {
         this.events = this.eventService.getEvents()
+    }
+    
+    handleThumbnailClick(eventName: any) {
+        toastr.success(eventName)
     }
 }
