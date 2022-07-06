5ece9fe0ab8924f11348023db9a62072030bdcbb
Blaire <ogoh_blessing@yahoo.com>
2022-07-06 17:11:53 +0100

    Lazy loading feature

--- src/routes.ts
+++ src/routes.ts
@@ -10,5 +10,9 @@ export const appRoutes:Routes = [
     { path: 'events', component: EventsListComponent },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
     { path: '404', component: Error404Component },
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { 
+        path: 'user', 
+        loadChildren: () => import('./app/user/user.module').then(m => m.UserModule)
+    }
 ]
